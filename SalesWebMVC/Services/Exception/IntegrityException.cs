79321c120e1785b889eb9ee21766e01095860c7f
Joao Pedro <jguedespresto@gmail.com>
2019-02-14 15:07:02 -0200

    Exception handling for delete (referencial integrity)

---
+++ SalesWebMVC/Services/Exception/IntegrityException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SalesWebMVC.Services.Exception {
+    public class IntegrityException : ApplicationException {
+
+        public IntegrityException(string message) : base(message) { }
+
+    }
+}
